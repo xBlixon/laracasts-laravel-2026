76014c3039fc5a27d586d12600885199c8215bb8
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-02-20 00:24:47 +0100

    Quotes!!!

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,13 +9,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>{{ $title }}</title>
 </head>
-<body>
+<body class="bg-gray-800 p-6 max-w-xl mx-auto text-white">
     <nav>
         <a href="/">Home</a>
         <a href="/about">About us</a>
         <a href="/contact">Contact us</a>
+        <a href="/quote">Quote</a>
     </nav>
     <main>
         {{ $slot }}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,29 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home',
-    [
-    'name' => request('name', "Guest"),
-    ]
+Route::get('/', function () {
+    return view('home',
+        [
+            'name' => request('name', "Guest"),
+        ]);
+}
+
 )->name('home');
 
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
+Route::get('/quote', function () {
+    $myQuotes = session()->get('quotes', []);
+    return view('quote',
+        [
+            'myQuotes' => $myQuotes
+        ]);
+})->name('quote');
+
+Route::post('/quote', function () {
+    $quote = request('quote');
+    session()->push('quotes', $quote);
+    return redirect('/quote');
+})->name('quote_post');
