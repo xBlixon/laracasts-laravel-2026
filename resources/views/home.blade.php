892c6619e7f5971be4082be7b21532fd3a1d402d
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-02-19 22:55:08 +0100

    Added the default name on the welcome page variable-ized by the query string "name" parameter

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Welcome">
-<h1>Welcome!</h1>
+<h1>Welcome {{ $name }}!</h1>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
+Route::view('/', 'home',
+    [
+    'name' => request('name', "Guest"),
+    ]
+)->name('home');
+
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
