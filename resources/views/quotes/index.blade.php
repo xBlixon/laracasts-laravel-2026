6a0a02bb30963ab2bc00852455f2f1ee4c5d00b9
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-02-23 00:34:08 +0100

    New quote link on index

--- resources/views/quotes/index.blade.php
+++ resources/views/quotes/index.blade.php
@@ -1,15 +1,16 @@
 <x-layout>
     @if($quotes->count())
-    <div class="mt-6">
-        <h2 class="font-bold text-2xl">Quotes</h2>
-        <ul class="mt-2">
-            @foreach($quotes as $quote)
-                <li class="mt-1">
-                    <a href="/quote/{{$quote->id}}/edit">{{$quote->text}}</a>
-                </li>
-            @endforeach
-        </ul>
-    </div>
+        <a href="/quote/create" class="mt-5 block">Make a new quote</a>
+        <div class="mt-6">
+            <h2 class="font-bold text-2xl">Quotes</h2>
+            <ul class="mt-2">
+                @foreach($quotes as $quote)
+                    <li class="mt-1">
+                        <a href="/quote/{{$quote->id}}/edit">{{$quote->text}}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     @else
         <h1 class="text-2xl mt-5">No quotes exist. <a href="/quote/create" class="underline accent-blue-300">Be the pioneer!</a></h1>
     @endif
