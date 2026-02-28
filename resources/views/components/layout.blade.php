3bbc5f36093cf6c3b0b2fbbefa13e97f256bc9e8
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-02-28 20:55:25 +0100

    Moving to vite asset bundler

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,9 +9,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{ $title }}</title>
 </head>
 <body>
